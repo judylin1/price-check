1f2884a7899e8e19a45e5f6f8b686c89d1683c02
Judy Lin <judith.s.lin@gmail.com>
2016-05-29 14:05:08 -0500

    heroku

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/price-checker',
+  mongoURL: process.env.MONGODB_URI || 'mongodb://localhost:27017/price-checker',
   port: process.env.PORT || 8000,
 };
 
